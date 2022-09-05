7134337544d070b0e2baf6309873ed895b28bcbd
Ramin <r.rezaie@digikala.com>
2022-09-06 03:26:01 +0430

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ yarn run build
 cd dist
 echo 'ramiiin.ir' > CNAME
 git init
-git add -A
+git add .
 git commit -m 'Deploy'
-git push -f https://github.com/raminr77/ramiiin.git main:production
+git push -f https://github.com/raminr77/ramiiin.git master:production
 cd -
