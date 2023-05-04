915d459a74be206deaca20dc79b7560b19948c38
Ramin <r.rezaie@digikala.com>
2023-05-04 10:28:02 +0330

    feat: index file for working mode

--- README.md
+++
@@ -1,5 +0,0 @@
-# Ramiiin
-Do You Know Ramin?
-<br />
-this project is a fun landing page for me and can help you to know me.
-- [Demo](https://ramiiin.ir/)
--- src/utils/scrollToEnd.js
+++
@@ -1,4 +0,0 @@
-export const scrollToEnd = () => {
-  const MESSAGES_ELEMENT = document.getElementById('messages');
-  MESSAGES_ELEMENT.scrollTop = MESSAGES_ELEMENT.scrollHeight + 100;
-};
